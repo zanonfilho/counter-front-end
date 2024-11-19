77e7239c1e234130e06af164dfe70c401c216dc3
zanonfilho <zanon44@gmail.com>
2024-11-19 00:22:43 -0300

    Build Inicial

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
   const {
     contract_address,
     counter_value,
-    recent_sender,
-    owner_address,
+    // recent_sender,
+    // owner_address,
     contract_balance,
     sendIncrement,
     sendDeposit,
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,4 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.tsx'
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
-import React from 'react';
+//import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
